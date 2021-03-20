250751a977cfe5552151e75de19bf437e3254788
Nirmeet <nirmeet.baweja@gmail.com>
2021-03-20 11:09:08 +0000

    React Week 2 - Ex A

--- src/Logo.js
+++ src/Logo.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function Logo(props) {
+  const logWhenClicked = () => {
+    console.log("You just clicked the image!");
+  };
+
   return (
     <header>
       <h1>Welcome to the {props.appName}</h1>
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="A bird pokemon"
+        onClick={logWhenClicked}
       />
     </header>
   );
